0f6fb406cf4cbf8167460afdd7b36a7be8c02147
bjjtw <bjjtw@mail.com>
2016-10-10 10:36:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var vue = new Vue({
+    el: '#main',
+    data: {
+        name: '',
+        no: '',
+        start: '',
+        end: '',
+        com: '',
+        com_shot: '',
+    }
+});
+
+var params = location.href.split('?')[1]
+
+if (params != null) {
+    var arr = params.split('&');
+    var i = 0
+    for (; i < arr.length; i++) {
+        kv = arr[i].split('=');
+        vue.$set(kv[0], kv[1]);
+    }
+}
+
